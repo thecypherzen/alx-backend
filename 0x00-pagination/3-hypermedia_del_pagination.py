fab93b05f26782cf66bb59eacc5bb388e7157778
William Inyam <sw.inyam@outlook.com>
2024-10-28 08:05:25 +0000

    revert(0x00:3): restore method signature annotation

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -39,7 +39,7 @@ class Server:
             }
         return self.__indexed_dataset
 
-    def get_hyper_index(self, index: Optional[int] = None,
+    def get_hyper_index(self, index: int = None,
                         page_size: int = 10) -> Dict:
         """Enforces data integrity after deletes
 
