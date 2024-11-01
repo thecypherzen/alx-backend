31c9f9cf3e545a5265903a24d2aa118d3f7843e4
William Inyam <sw.inyam@outlook.com>
2024-11-01 17:05:13 +0000

    build(0x01:0): add basiccache method, testfile and readme

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""A caching system"""
+
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """Defines a Basic Cache"""
+
+    def __init__(self):
+        """initialises this cache"""
+        super().__init__()
+
+    def put(self, key, item):
+        """Adds an item to cache
+        If key or item is None, does nothing
+        """
+        if any([not key, not item]):
+            pass
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """Returns value in cace at key
+        If key is None or doesn't exist in dictionary, returns
+        None
+        """
+        if key:
+            return self.cache_data.get(key)
+        return None
