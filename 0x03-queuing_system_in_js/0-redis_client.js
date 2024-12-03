50811e3522dce4ab0559ed194bcc305fa9251b64
William Inyam <sw.inyam@outlook.com>
2024-12-03 11:36:40 +0000

    build(0x03:1): add redis_client setup

---
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,11 @@
+import { createClient } from 'redis';
+
+/* eslint-disable */
+const client = createClient()
+      .on('error', (err) => {
+        console.log(
+          `Redis client not connected to the server: ${err.message}`);
+      }).connect()
+      .then(() => {
+        console.log('Redis client connected to the server');
+      });
